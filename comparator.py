64abf679d6f7f32badc3210ee4b89b6d1e852476
Melissa Gray <61853436+mag535@users.noreply.github.com>
2020-08-18 14:21:04 -0400

    update 3
    added class Comparator and moved functions inside

--- comparator.py
+++ comparator.py
@@ -17,194 +17,199 @@ Data Tree:
 '''
 
 
-#%% FUNCTIONS
+#%% CLASS
 
-def get_tax_ID(sample):
-    '''
+class Comparator():
+    def __init__(self):
+        return
+    def get_tax_ID(self, sample):
+        '''
     
-    Parameters
-    ----------
-    sample : dictionary
-        where taxID is the key and abundance is the value
+        Parameters
+        ----------
+        sample : dictionary
+            where taxID is the key and abundance is the value
 
-    Returns
-    -------
-    tax_id : set
-        filled with taxIDs from the dictionary sample
+        Returns
+        -------
+        tax_id : set
+            filled with taxIDs from the dictionary sample
 
-    '''
-    tax_id = set()
+        '''
+        tax_id = set()
     
-    for rank in sample:
-        for tid in sample[rank]:
-            tax_id.add(tid)
-    #print("done adding.")
-    return tax_id
+        for rank in sample:
+            for tid in sample[rank]:
+                tax_id.add(tid)
+        #print("done adding.")
+        return tax_id
 
-def save_tax_ID(samples):
-    '''
+    def save_tax_ID(self, samples):
+        '''
 
-    Parameters
-    ----------
-    samples : dictionary
-        where sample # is the key and a dictionary of {taxID : abundance} is
-        the value
+        Parameters
+        ----------
+        samples : dictionary
+            where sample # is the key and a dictionary of {taxID : abundance} is
+            the value
 
-    Returns
-    -------
-    taxIDs : dictionary
-        where sample # is the key and a set of taxIDs from that sample is the 
-        value
+        Returns
+        -------
+        taxIDs : dictionary
+            where sample # is the key and a set of taxIDs from that sample is the 
+            value
 
-    '''
-    taxIDs = {}
+        '''
+        taxIDs = {}
     
-    for sample_num in samples:
-        taxIDs[sample_num] = get_tax_ID(samples[sample_num])
-    #print("done indexing.")
-    return taxIDs
+        for sample_num in samples:
+            taxIDs[sample_num] = self.get_tax_ID(samples[sample_num])
+        #print("done indexing.")
+        return taxIDs
 
-def _common_tax_ID(tax_id_1, tax_id_2):
-    '''
+    def _common_tax_ID(self, tax_id_1, tax_id_2):
+        '''
     
-    Parameters
-    ----------
-    tax_id_1 : set
-        of TaxIDs from a sample
-    tax_id_2 : set
-        of TaxIDs from a sample
+        Parameters
+        ----------
+        tax_id_1 : set
+            of TaxIDs from a sample
+        tax_id_2 : set
+            of TaxIDs from a sample
 
-    Returns
-    -------
-    set
-        with the values both sets share
+        Returns
+        -------
+        set
+            with the values both sets share
 
-    '''
-    #print("done comparing.")
-    return tax_id_1 & tax_id_2
+        '''
+        #print("done comparing.")
+        return tax_id_1 & tax_id_2
 
-def common_tax_ID(tax_id_1, tax_id_2):
-    '''
+    def common_tax_ID(self, tax_id_1, tax_id_2):
+        '''
 
-    Parameters
-    ----------
-    tax_id_1 : dictionary
-        where sample # is the key and a set of taxIDs from that sample is the 
-        value
-    tax_id_2 : dictionary
-        where sample # is the key and a set of taxIDs from that sample is the 
-        value
+        Parameters
+        ----------
+        tax_id_1 : dictionary
+            where sample # is the key and a set of taxIDs from that sample is the 
+            value
+        tax_id_2 : dictionary
+            where sample # is the key and a set of taxIDs from that sample is the 
+            value
 
-    Returns
-    -------
-    common_data_points : dictionary
-        where the sample # is the key and a set with the overlapping data points
-        between the two samples is the value
+        Returns
+        -------
+        common_data_points : dictionary
+            where the sample # is the key and a set with the overlapping data points
+            between the two samples is the value
 
-    '''
-    common_data_points = {}
+        '''
+        common_data_points = {}
     
-    for n in tax_id_1:
-        common_data_points[n] = _common_tax_ID(tax_id_1[n], tax_id_2[n])
+        for n in tax_id_1:
+            common_data_points[n] = self._common_tax_ID(tax_id_1[n], tax_id_2[n])
     
-    return common_data_points
+        return common_data_points
 
-def _combine_tax_ID(tax_id_1, tax_id_2):
-    '''
+    def _combine_tax_ID(self, tax_id_1, tax_id_2):
+        '''
 
-    Parameters
-    ----------
-    tax_id_1 : set
-        with TaxIDs from one sample
-    tax_id_2 : set
-        with TaxIDs from one sample
+        Parameters
+        ----------
+        tax_id_1 : set
+            with TaxIDs from one sample
+        tax_id_2 : set
+            with TaxIDs from one sample
 
-    Returns
-    -------
-    set
-        combined set of t1 and t2 with no repeated values
+        Returns
+        -------
+        set
+            combined set of t1 and t2 with no repeated values
 
-    '''
-    #print("done combining.")
-    return tax_id_1 | tax_id_2
+        '''
+        #print("done combining.")
+        return tax_id_1 | tax_id_2
 
-def combine_tax_ID(tax_id_1, tax_id_2):
-    '''
+    def combine_tax_ID(self, tax_id_1, tax_id_2):
+        '''
 
-    Parameters
-    ----------
-    tax_id_1 : dictionary
-        where sample # is the key and a set with TaxIDs from that sample is 
-        the value
-    tax_id_2 : dictionary
-        where sample # is the key and a set with TaxIDs from that sample is 
-        the value
+        Parameters
+        ----------
+        tax_id_1 : dictionary
+            where sample # is the key and a set with TaxIDs from that sample is 
+            the value
+        tax_id_2 : dictionary
+            where sample # is the key and a set with TaxIDs from that sample is 
+            the value
 
-    Returns
-    -------
-    combined_tax_IDs : dictionary
-        where the sample # is the key and a set of the combined data points 
-        (with no repeats) is the value
+        Returns
+        -------
+        combined_tax_IDs : dictionary
+            where the sample # is the key and a set of the combined data points 
+            (with no repeats) is the value
 
-    '''
-    combined_tax_IDs = {}
+        '''
+        combined_tax_IDs = {}
     
-    for n in tax_id_1:
-        combined_tax_IDs[n] = (_combine_tax_ID(tax_id_1[n], tax_id_2[n]))
-    #print("done listing")
-    return combined_tax_IDs
+        for n in tax_id_1:
+            combined_tax_IDs[n] = self._combine_tax_ID(tax_id_1[n], tax_id_2[n])
+        #print("done listing")
+        return combined_tax_IDs
 
-def main(file1, file2, t=0):
-    '''
+    def main(self, file1, file2, t=0):
+        Chai = pp.Parser()
+        '''
 
-    Parameters
-    ----------
-    file1 : string
-        the profile file name before ".profile"
-    file2 : string
-        the profile file name before ".profile"
+        Parameters
+        ----------
+        file1 : string
+            the profile file name before ".profile"
+        file2 : string
+            the profile file name before ".profile"
 
-    Returns
-    -------
-    common_1_2: dictionary
-        where sample # is the key and a set of common data points between the 
-        two files is the value
-    combined_1_2: dictionary
-        where sample # is the key and a set of combined data points from the 
-        two files is the value (no repeats)
+        Returns
+        -------
+        common_1_2: dictionary
+            where sample # is the key and a set of common data points between the 
+            two files is the value
+        combined_1_2: dictionary
+            where sample # is the key and a set of combined data points from the 
+            two files is the value (no repeats)
 
-    '''
-    Sample1 = pp.main(file1, t)
-    Sample2 = pp.main(file2, t)
+        '''
+        Sample1 = Chai.main(file1, t)
+        Sample2 = Chai.main(file2, t)
     
-    t1 = save_tax_ID(Sample1)
-    t2 = save_tax_ID(Sample2)
+        t1 = self.save_tax_ID(Sample1)
+        t2 = self.save_tax_ID(Sample2)
     
-    common_1_2 = common_tax_ID(t1, t2)
-    combined_1_2 = combine_tax_ID(t1, t2)
-    #print("done (comp).")
-    return common_1_2, combined_1_2
+        common_1_2 = self.common_tax_ID(t1, t2)
+        combined_1_2 = self.combine_tax_ID(t1, t2)
+        #print("done (comp).")
+        return common_1_2, combined_1_2
 
-def print_tax_ID(tax_id):
-    '''
+    def print_tax_ID(self, tax_id):
+        '''
 
-    Parameters
-    ----------
-    tax_id : dictionary
-        where sample # is the key and a set with taxIDs is the value
+        Parameters
+        ----------
+        tax_id : dictionary
+            where sample # is the key and a set with taxIDs is the value
 
-    Returns
-    -------
-    None.
+        Returns
+        -------
+        None.
 
-    '''
-    for n in tax_id:
-        print("Sample Number:", n)
-        for t in tax_id[n]:
-            print("\t\t{}".format(t))
-    return
+        '''
+        for n in tax_id:
+            print("Sample Number:", n)
+            for t in tax_id[n]:
+                print("\t\t{}".format(t))
+        return
 
 def example():
+    Chip = Comparator()
     '''
 
     Returns
@@ -217,29 +222,34 @@ def example():
     b = {1 : {1,3,4}, 2 : {4,5,6}}
     print("b - ", b, "\n")
     
-    ab = common_tax_ID(a, b)
-    a_b  = combine_tax_ID(a, b)
+    ab = Chip.common_tax_ID(a, b)
+    a_b  = Chip.combine_tax_ID(a, b)
     print("COMMON:")
-    print_tax_ID(ab)
+    Chip.print_tax_ID(ab)
     print()
     print("COMBINED")
-    print_tax_ID(a_b)
+    Chip.print_tax_ID(a_b)
     return
 
 
 #%% MAIN
 
 if __name__ == "__main__":
+    '''
+    Tea = Comparator()
+    Chai = pp.Parser()
+    
     f = input("Which two files do you want to compare \n (only type file name before \'.profile\' and separate by space):\n")
     f_2 = f.split(" ")
-    S1 = pp.main(f_2[0], 0)
-    S2 = pp.main(f_2[1], 0)
+    S1 = Chai.main(f_2[0], 0)
+    S2 = Chai.main(f_2[1], 0)
     
-    t1 = save_tax_ID(S1)
-    t2 = save_tax_ID(S2)
+    t1 = Tea.save_tax_ID(S1)
+    t2 = Tea.save_tax_ID(S2)
     
-    comT = common_tax_ID(t1, t2)
-    combT = combine_tax_ID(t1, t2)
+    comT = Tea.common_tax_ID(t1, t2)
+    combT = Tea.combine_tax_ID(t1, t2)
+    '''
     
     #example()
     
