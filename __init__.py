a04dae34fa27f712218438e4fa6298022f900846
Melissa Gray <61853436+mag535@users.noreply.github.com>
2020-08-18 14:22:20 -0400

    for packaging

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 17 16:58:22 2020
+
+@author: Melissa Gray
+"""
+
+from profile_parser import Parser
+from comparator import Comparator
+from confusion_matrix import Confusion
