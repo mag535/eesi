47488379d418a20105476ee1c2bacec11669bd73
Melissa Gray <61853436+mag535@users.noreply.github.com>
2020-08-26 11:12:41 -0400

    MoC package

--- __init__.py
+++ __init__.py
@@ -8,3 +8,4 @@ Created on Mon Aug 17 16:58:22 2020
 from MoC.profile_parser import Parser
 from MoC.comparator import Comparator
 from MoC.confusion_matrix import Confusion
+from MoC.precall import Misc
